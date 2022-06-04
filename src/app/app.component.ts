3ac9a7cf29170b53825378f9bb3a10db3c5ad322
JitBherwani <jitbherwani92@gmail.com>
2022-06-04 13:43:42 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DateFormControl } from './date-form-control';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  cardForm! :FormGroup;  
+  submitted = false;
   title = 'creditcard';
+
+  constructor(private formBuilder:FormBuilder){}
+
+  ngOnInit(){
+    this.cardForm = this.formBuilder.group({
+      firstName : ['', Validators.required],
+      // lastName : ['', Validators.required],
+      CardNumber : ['', [Validators.required,Validators.minLength(16),Validators.maxLength(16),]],
+      Expiration :new DateFormControl ('', [Validators.required,Validators.pattern(/^(0[1-9]|1[0-2]\/\d{2}$)/)]),
+      // Email : ['', [Validators.required,Validators.email]],
+      SecurityCode : ['', [Validators.required,Validators.minLength(3),Validators.maxLength(3)]]
+    })
+  }
+
+  onSubmit(){
+    
+    this.submitted = true
+    if (this.cardForm.invalid) {
+      return
+    }
+    alert("Submitted")
+  }
+
+  onReset(){
+    this.cardForm.reset();
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';  
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CardComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
